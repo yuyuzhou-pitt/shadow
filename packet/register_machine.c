6403f4cf441f89fe43dfb2817060226bd67df750
root <yuyuzhouchina@gmail.com>
2016-06-29 15:58:45 -0400

    add getRegisterPort

--- packet/register_machine.c
+++ packet/register_machine.c
@@ -27,6 +27,102 @@ int writeRegisterMachine(Packet *packet_recv, char *file){
     AppProcess *app_process;
     app_process = (AppProcess *)malloc(sizeof(app_process));
 
+    Machine *machine = &(packet_recv->Data.app_process.machine[0]);
+
+    char hostfile[17];
+    memset(hostfile, 0, sizeof(hostfile));
+
+    strcpy(hostfile, "../");
+    strcat(hostfile, file);
+
+    int file_exists = 1;
+    if(access(hostfile, F_OK) < 0) {
+        char logmsg[128]; snprintf(logmsg, sizeof(logmsg), "readfile: File not found: %s\n", hostfile);
+        logging(LOGFILE, logmsg);
+        file_exists = 0;
+    }
+
+    /*printable str*/
+    memset(registerString, 0, sizeof(registerString));
+    snprintf(registerString, sizeof(registerString), "%s:%d\n", \
+         packet_recv->sender_ip, machine->port);
+
+    /*check duplication before writing*/
+
+    int dup_number = 0; //record the duplication numbers
+    int server_exists = checkDup(machine, hostfile);
+    /*do not check dup if file do not exists*/
+
+    if (server_exists == 1){
+        printf("Duplicate registration: %s", registerString);
+        dup_number ++;
+        //continue;
+    }
+
+    if(writeFile(registerString, strlen(registerString), hostfile, "a") < 0){
+        perror("writeport");
+        return -1;
+    }
+
+    char logmsg[128]; snprintf(logmsg, sizeof(logmsg), "write register into register machine table. \n");
+    logging(LOGFILE, logmsg);
+
+
+    return server_exists;
+}
+
+int checkDup(Machine *machine, char *file){
+    int file_exists = 1;
+    if(access(file, F_OK) < 0) {
+        char logmsg[128]; snprintf(logmsg, sizeof(logmsg), "readfile: File not found: %s\n", file);
+        logging(LOGFILE, logmsg);
+        file_exists = 0;
+    }
+
+    char *tempfile = ".tempfile";
+
+    if (file_exists == 1){
+        if(copy_file(file, tempfile) != 0)
+            fprintf(stderr, "Error during copy!");
+    }
+
+    /*check duplication before writing*/
+
+    FILE *table_fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    int server_exists = 0;
+    /*do not check dup if file do not exists*/
+    if(file_exists == 1) {
+        if ((table_fp = fopen(tempfile,"r")) < 0){
+            char logmsg[128]; snprintf(logmsg, sizeof(logmsg), "readfile: Failed to open file: %s\n", tempfile);
+            logging(LOGFILE, logmsg);
+            return -1;
+        }
+
+        while ((read = getline(&line, &len, table_fp)) != -1) {
+            char *manager_ip, *manager_port;
+            manager_ip = strtok_r(line, ":", &manager_port);
+
+            /*to find the register info which match the request program_name, version_number, and procedure_name*/
+            if(strcmp(manager_ip, machine->ip) == 0 &&
+               atoi(manager_port) == machine->port ){
+                server_exists = 1;
+                break; //jump out the while loop
+            }
+        }
+        if (line) free(line);
+        fclose(table_fp);
+    }
+    unlinkFile(tempfile);
+    return server_exists;
+}
+
+int getRegisterPort(char *machine_ip, char *file){
+    int port;
+
     char hostfile[17];
     memset(hostfile, 0, sizeof(hostfile));
 
@@ -47,62 +143,34 @@ int writeRegisterMachine(Packet *packet_recv, char *file){
             fprintf(stderr, "Error during copy!");
     }
 
-    int manager_exists = 0; //for sending hello if it's a new manager
-    int dup_number = 0; //record the duplication numbers
-    int i;
-    //for(i=0;i<packet_recv->Data.procedure_number;i++){
-        /*printable str*/
-        memset(registerString, 0, sizeof(registerString));
-        snprintf(registerString, sizeof(registerString), "%s:%d\n", \
-             packet_recv->sender_ip, packet_recv->Data.app_process.main.port);
+    /*check duplication before writing*/
 
-        /*check duplication before writing*/
+    FILE *table_fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
-        FILE *table_fp;
-        char *line = NULL;
-        size_t len = 0;
-        ssize_t read;
-
-        int server_exists = 0;
-        /*do not check dup if file do not exists*/
-        if(file_exists == 1) {
-            if ((table_fp = fopen(tempfile,"r")) < 0){
-                char logmsg[128]; snprintf(logmsg, sizeof(logmsg), "readfile: Failed to open file: %s\n", tempfile);
-                logging(LOGFILE, logmsg);
-                return -1;
-            }
-
-            while ((read = getline(&line, &len, table_fp)) != -1) {
-                char *manager_ip, *manager_port;
-                manager_ip = strtok_r(line, ":", &manager_port);
-
-                /*to find the register info which match the request program_name, version_number, and procedure_name*/
-                if(strcmp(manager_ip, packet_recv->Data.app_process.main.ip) == 0 &&
-                   atoi(manager_port) == packet_recv->Data.app_process.main.port ){
-                    server_exists = 1;
-                    break; //jump out the while loop
-                }
-            }
-            if (line) free(line);
-            fclose(table_fp);
-        }
-
-        if (server_exists == 1){
-            printf("Duplicate registration: %s", registerString);
-            dup_number ++;
-            //continue;
-        }
-
-        if(writeFile(registerString, strlen(registerString), hostfile, "a") < 0){
-            perror("writeport");
+    /*do not check dup if file do not exists*/
+    if(file_exists == 1) {
+        if ((table_fp = fopen(tempfile,"r")) < 0){
+            char logmsg[128]; snprintf(logmsg, sizeof(logmsg), "readfile: Failed to open file: %s\n", tempfile);
+            logging(LOGFILE, logmsg);
             return -1;
         }
 
-        char logmsg[128]; snprintf(logmsg, sizeof(logmsg), "write register into register machine table. \n");
-        logging(LOGFILE, logmsg);
+        while ((read = getline(&line, &len, table_fp)) != -1) {
+            char *manager_ip, *manager_port;
+            manager_ip = strtok_r(line, ":", &manager_port);
 
-    //}
-
+            /*to find the register info which match the request program_name, version_number, and procedure_name*/
+            if(strcmp(manager_ip, machine_ip) == 0){
+                port = manager_port;
+                break; //jump out the while loop
+            }
+        }
+        if (line) free(line);
+        fclose(table_fp);
+    }
     unlinkFile(tempfile);
-    return manager_exists;
+    return port;
 }
