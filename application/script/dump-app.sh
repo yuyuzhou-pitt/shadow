027c63b009f046b4849cfa713d738dc709b9b83c
root <yuyuzhouchina@gmail.com>
2016-07-02 16:45:40 -0400

    update the pid directory

--- application/script/dump-app.sh
+++ application/script/dump-app.sh
@@ -2,6 +2,7 @@
 # echo -e "\n\n\n" | ssh-keygen -t rsa
 # cat ~/.ssh/id_rsa.pub | ssh root@192.168.0.104 'cat >> ~/.ssh/authorized_keys'
 
+DUMP_DIR=`hostname -i | awk '{print $NF}'`
 if [[ $# -lt 2 ]];then
     echo "USAGE: dump-app.sh <full-app-path> <shadow-ip:/tmp/dump_$pid>"
     echo "Example: dump-app.sh /root/shadow/application/pi 10.10.2.205:/tmp/dump_4567"
@@ -9,9 +10,9 @@ if [[ $# -lt 2 ]];then
 fi
 pid=$(ps aux |grep "$1" | head -n 1 | awk '{print $2}')
 echo $pid
-mkdir "dump_$pid"
-cd "dump_$pid"
-criu dump -t "dump_$pid" -vvv -o dump.log --shell-job --prev-images-dir ../"dump_$pid" --leave-running  && echo dump OK
+mkdir -p "$DUMP_DIR"
+cd "$DUMP_DIR"
+criu dump -t "$pid" -vvv -o dump.log --shell-job --prev-images-dir ../"$DUMP_DIR" --leave-running  && echo dump OK
 cd ..
 # overwrite target directory
-scp -prq "dump_$pid/." $2
+scp -r "$DUMP_DIR" $2
--- application/script/restore-app.sh
+++ application/script/restore-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $# -lt 1 ]];then
-    echo "USAGE: restore-app.sh </tmp/dump_$pid>"
-    echo "Example: restore-app.sh /tmp/dump_4567"
+    echo "USAGE: restore-app.sh /tmp/<main-ip>"
+    echo "Example: restore-app.sh /tmp/192.168.0.107
     exit -1;
 fi
 criu restore --tree $1 --images-dir ./$1 -vvvv -o restore.log --shell-job && echo restore OK
