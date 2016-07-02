e063a50a8392fddc2e419fefbea6c1d69c5c4f88
root <yuyuzhouchina@gmail.com>
2016-07-02 19:10:19 -0400

    wait pid file before leap

--- application/script/dump-app.sh
+++ application/script/dump-app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # echo -e "\n\n\n" | ssh-keygen -t rsa
 # cat ~/.ssh/id_rsa.pub | ssh root@192.168.0.104 'cat >> ~/.ssh/authorized_keys'
+PID_FILE=../.1_dump_pid
+rm -rf $PID_FILE
 
 DUMP_DIR=`hostname -i | awk '{print $NF}'`
 if [[ $# -lt 2 ]];then
@@ -9,10 +11,10 @@ if [[ $# -lt 2 ]];then
     exit -1;
 fi
 pid=$(ps aux |grep "$1" | head -n 1 | awk '{print $2}')
-echo $pid > ../.dump_pid
 mkdir -p "$pid"-2
 cd "$pid"-2
 criu dump -t "$pid"-2 -vvv -o dump.log --shell-job --prev-images-dir ../"$pid" --leave-running  && echo dump OK
 cd ..
 # overwrite target directory
 scp -r "$pid"-2 $2
+echo $pid > $PID_FILE
