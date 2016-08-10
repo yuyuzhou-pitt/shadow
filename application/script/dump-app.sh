127acad7103af548a2abf2ef6ec425bc97c2ec81
root <yuyuzhouchina@gmail.com>
2016-08-10 13:20:53 -0400

    remove prev-images in criu cmd

--- application/script/dump-app.sh
+++ application/script/dump-app.sh
@@ -10,18 +10,18 @@ timestamp() {
 
 DUMP_DIR=`hostname -i | awk '{print $NF}'`
 if [[ $# -lt 2 ]];then
-    echo "USAGE: dump-app.sh <full-app-path> <shadow-ip:/tmp/dump_$pid>"
-    echo "Example: dump-app.sh /root/shadow/application/pi 10.10.2.205:/tmp/dump_4567"
+    echo "USAGE: dump-app.sh <full-app-path> <shadow-ip:/tmp/>"
+    echo "Example: dump-app.sh /root/shadow/application/pi 192.168.0.113:/tmp/"
     exit -1;
 fi
-pid=$(ps aux |grep "$1" | head -n 1 | awk '{print $2}')
+pid=$(ps aux |grep "$1" | grep -v 'criu' | head -n 1 | awk '{print $2}')
 mkdir -p "$pid"
 cd "$pid"
 
 printf "+--BEFORE-DUMP-------\n"
 timestamp 
 printf "+-------------------+\n"
-criu dump -t "$pid" -vvv -o dump.log --shell-job --prev-images-dir ../"$pid" --leave-running  && echo dump OK
+criu dump -t "$pid" -vvv -o dump.log --shell-job --leave-running  && echo dump OK
 printf "+--AFTER-DUMP-------+\n"
 timestamp 
 printf "+-------------------+\n"
